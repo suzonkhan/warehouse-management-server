4764949b5c86159c8297102fd6cc2d4d550879c9
Abdur Rahman Suzon <abdur.rahman@egeneration.co>
2022-05-09 20:51:46 +0600

    update my product api

--- index.js
+++ index.js
@@ -3,11 +3,31 @@ const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
+var jwt = require('jsonwebtoken');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    // console.log(token);
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' });
+        }
+        // console.log('decoded', decoded);
+        req.decoded = decoded;
+        next();
+    })
+}
+
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tl4lz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -19,6 +39,14 @@ async function run(){
         await client.connect();
         const productCollection = client.db("warehouse").collection("products");
 
+        app.post('/login', async(req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            })
+            res.send({accessToken})
+        })
+        // Add product 
         app.post('/product', async(req, res)=>{ 
             const newProduct = req.body; 
             const result = await productCollection.insertOne(newProduct);
@@ -26,6 +54,7 @@ async function run(){
             res.send(result); 
         })
 
+        // View products 
         app.get('/products', async (req, res)=>{
             const query = { }; 
             const cursor = productCollection.find(query);
@@ -33,21 +62,23 @@ async function run(){
             res.send(products);
         })
 
-        app.get('/products/:email', async (req, res)=>{
-            const email = req.params.email;
-            const query = {userEmail: email };  
+        // View products collection (My Products) 
+        app.get('/product', async (req, res)=>{ 
+            const email = req.query.email; 
+            const query = {userEmail: email }; 
             const cursor = productCollection.find(query);
-            const products =   await cursor.toArray();
+            const products = await cursor.toArray();
             res.send(products);
         })
 
+        //Load Single Product
         app.get('/product/:id', async(req, res)=>{
             const id = req.params.id;
              const query = { _id: ObjectId(id) };
             const product = await productCollection.findOne(query);
             res.send(product);
         })
-
+        //Update Single Product
         app.put('/product/:id', async(req, res)=>{
             const id = req.params.id;
             const udpatedProduct = req.body;
@@ -65,7 +96,7 @@ async function run(){
             res.send(result);
 
         })
-
+        //Delete Single Product
         app.delete('/product/:id', async (req, res)=>{
             const id = req.params.id; 
             const query = { _id: ObjectId(id) };
